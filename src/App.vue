0714f8adc95d930a0467b8dce404ac422284a27a
1520940216 <1520940216@qq.com>
2019-08-01 19:51:39 +0800

    try

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+   App <el-button type="success">成功按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
